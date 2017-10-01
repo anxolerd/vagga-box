8de8b2a7facc6714efb486eb01c416327aac05b5
Oleksandr Kovalchuk <me.olexandr.kovalchuk@gmail.com>
2017-10-01 15:08:01 +0300

    Modify upgrade script to handle params
    Now upgrade-vagga.sh can upgrade either to stable or to testing versions
    of vagga depending on arguments.
    By default upgrade to stable version of vagga as testing version by
    definition may contain unexpected bugs and be unstable.

--- image/http/upgrade-vagga.sh
+++ image/http/upgrade-vagga.sh
@@ -1,2 +1,17 @@
 #!/bin/sh
-curl -sfS http://files.zerogw.com/vagga/vagga-install-testing.sh | sh
+
+case "$1" in
+    # If no arguments provided update to the latest stable version
+    ''|--stable)
+        install_script="vagga-install.sh"
+        ;;
+    --testing)
+        install_script="vagga-install-testing.sh"
+        ;;
+    *)
+        echo 'Specify one of "--stable", "--testing"' >&2
+        exit 1
+        ;;
+esac
+
+curl -sfS "http://files.zerogw.com/vagga/${install_script}" | sh
